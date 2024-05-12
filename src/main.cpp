8f3f85e5a1f43393eeb5394e9fab283cb5102c60
Paulina Gąsiorowska <paulina.gasiorowska@icloud.com>
2024-05-12 16:54:54 +0200

    my simple app

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,22 @@
 
 #include "code.hpp"
 
-int main(int argc, char** argv) {
-  // TODO: main program function
+int main() {
+   int liczba1, liczba2, suma;
+
+   // Pytanie o pierwszą liczbę
+   std::cout << "Podaj pierwsza liczbe calkowita: ";
+   std::cin >> liczba1;
+
+   // Pytanie o drugą liczbę
+   std::cout << "Podaj druga liczbe calkowita: ";
+   std::cin >> liczba2;
+
+   // Dodawanie liczb
+   suma = liczba1 + liczba2;
+
+   // Wyświetlenie wyniku
+   std::cout << "Suma liczb " << liczba1 << " i " << liczba2 << " wynosi: " << suma << std::endl;
+
+   return 0;
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,34 @@
 #include "../src/code.hpp"
+#include <iostream>
+#include <cassert>
+
+// Funkcja dodająca dwie liczby całkowite
+int dodaj(int a, int b) {
+   return a + b;
+}
+
+// Test
+void testDodawania() {
+   // Test 1: Dodawanie dwóch dodatnich liczb
+   assert(dodaj(5, 10) == 15);
+
+   // Test 2: Dodawanie dwóch ujemnych liczb
+   assert(dodaj(-5, -10) == -15);
+
+   // Test 3: Dodawanie dodatniej i ujemnej liczby
+   assert(dodaj(5, -10) == -5);
+
+   // Test 4: Dodawanie ujemnej i dodatniej liczby
+   assert(dodaj(-5, 10) == 5);
+
+   // Test 5: Dodawanie dwóch zer
+   assert(dodaj(0, 0) == 0);
+
+   std::cout << "Testy zakończone pomyślnie!" << std::endl;
+}
 
 int main() {
-  // TODO: test code goes here
+   testDodawania();
+
+   return 0;
 }
